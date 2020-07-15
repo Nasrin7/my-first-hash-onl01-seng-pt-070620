e574bcf21affb5d6fd4878562777184a272d7946
Nasrin Rahimi <77.nasrin@gmail.com>
2020-07-15 11:05:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,7 +14,7 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-                 binding.pry
+              
   shipping_manifest["oil paintings"]
 end
 
